8558256f3e2b27af74e8c89de65a129a8cc09463
glencode <glenkingoina@gmail.com>
2025-05-12 15:27:55 +0300

    Auto-commit: Changes detected on 2025-05-12 15:27:54

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<!-- <main id="primary" class="site-main"> -->
 
 		<section class="error-404 not-found py-5 text-center">
 			<div class="container">
